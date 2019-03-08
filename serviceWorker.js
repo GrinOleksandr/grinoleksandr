ddbd9e05b33cb831f93900e41560b87609d3f725
Oleksandr Grin <44158680+GrinOleksandr@users.noreply.github.com>
2019-03-08 12:59:20 +0200

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,95 +1,95 @@
-let currentCacheName = 'myGitHub';
-
-let filesToCache = [
-    '/index.html',
-    '/js/main/js',
-    '/img/AjaxLoader.gif',
-    '/css/style.css',
-    '/css/reset.css'
-];
-
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(currentCacheName)
-            .then(function (cache) {
-                return cache.addAll(filesToCache)
-            })
-    );
-});
-
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
-        caches.keys()
-            .then(function (cachedNames) {
-                return Promise.all(
-                    cachedNames.filter(function (cacheName) {
-                        return cacheName !== currentCacheName;
-                    }).map(function (cacheName) {
-                        return caches.delete(cacheName);
-                    })
-                )
-            })
-    )
-});
-
-
-//FAST TRACK
-// self.addEventListener('install', (event)=>{
-//     event.waitUntil(self.skipWaiting());
-// });
+// let currentCacheName = 'myGitHub';
 //
+// let filesToCache = [
+//     '/index.html',
+//     '/js/main/js',
+//     '/img/AjaxLoader.gif',
+//     '/css/style.css',
+//     '/css/reset.css'
+// ];
 //
-// self.addEventListener('activate', (event)=>{
-//     event.waitUntil(self.clients.claim());
-// });
-//FAST TRACK END
-
-
-// self.addEventListener('fetch', function(event) {
-//     event.respondWith(
-//         caches.match(event.request)
-//             .then(function(response) {
-//                     // ресурс есть в кеше
-//                     if (response) {
-//                         return response;
-//                     }
-//                     return fetch(event.request);
-//                 }
-//             )
-//     );
-// });
-
-
-// self.addEventListener('fetch', function(event) {
-//     event.respondWith(
-//         caches.match(event.request)
-//             .then(function(response) {
-//                 // ресурс есть в кеше
-//                 if (response) {
-//                     return response;
-//                 }
-//
-//                 /* Важно: клонируем запрос. Запрос - это поток, может быть обработан только раз. Если мы хотим использовать объект request несколько раз, его нужно клонировать */
-//                 var fetchRequest = event.request.clone();
-//
-//                 return fetch(fetchRequest).then(
-//                     function(response) {
-//                         // проверяем, что получен корректный ответ
-//                         if(!response || response.status !== 200 || response.type !== 'basic') {
-//                             return response;
-//                         }
-//
-//                         /* ВАЖНО: Клонируем ответ. Объект response также является потоком. */
-//                         var responseToCache = response.clone();
-//
-//                         caches.open(CACHE_NAME)
-//                             .then(function(cache) {
-//                                 cache.put(event.request, responseToCache);
-//                             });
-//
-//                         return response;
-//                     }
-//                 );
+// self.addEventListener('install', function(event) {
+//     event.waitUntil(
+//         caches.open(currentCacheName)
+//             .then(function (cache) {
+//                 return cache.addAll(filesToCache)
 //             })
 //     );
 // });
+//
+// self.addEventListener('activate', function(event) {
+//     event.waitUntil(
+//         caches.keys()
+//             .then(function (cachedNames) {
+//                 return Promise.all(
+//                     cachedNames.filter(function (cacheName) {
+//                         return cacheName !== currentCacheName;
+//                     }).map(function (cacheName) {
+//                         return caches.delete(cacheName);
+//                     })
+//                 )
+//             })
+//     )
+// });
+//
+//
+// //FAST TRACK
+// // self.addEventListener('install', (event)=>{
+// //     event.waitUntil(self.skipWaiting());
+// // });
+// //
+// //
+// // self.addEventListener('activate', (event)=>{
+// //     event.waitUntil(self.clients.claim());
+// // });
+// //FAST TRACK END
+//
+//
+// // self.addEventListener('fetch', function(event) {
+// //     event.respondWith(
+// //         caches.match(event.request)
+// //             .then(function(response) {
+// //                     // ресурс есть в кеше
+// //                     if (response) {
+// //                         return response;
+// //                     }
+// //                     return fetch(event.request);
+// //                 }
+// //             )
+// //     );
+// // });
+//
+//
+// // self.addEventListener('fetch', function(event) {
+// //     event.respondWith(
+// //         caches.match(event.request)
+// //             .then(function(response) {
+// //                 // ресурс есть в кеше
+// //                 if (response) {
+// //                     return response;
+// //                 }
+// //
+// //                 /* Важно: клонируем запрос. Запрос - это поток, может быть обработан только раз. Если мы хотим использовать объект request несколько раз, его нужно клонировать */
+// //                 var fetchRequest = event.request.clone();
+// //
+// //                 return fetch(fetchRequest).then(
+// //                     function(response) {
+// //                         // проверяем, что получен корректный ответ
+// //                         if(!response || response.status !== 200 || response.type !== 'basic') {
+// //                             return response;
+// //                         }
+// //
+// //                         /* ВАЖНО: Клонируем ответ. Объект response также является потоком. */
+// //                         var responseToCache = response.clone();
+// //
+// //                         caches.open(CACHE_NAME)
+// //                             .then(function(cache) {
+// //                                 cache.put(event.request, responseToCache);
+// //                             });
+// //
+// //                         return response;
+// //                     }
+// //                 );
+// //             })
+// //     );
+// // });
