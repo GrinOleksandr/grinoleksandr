819db84a9e0c5c9cccea1e4c2bb9049a34f062f9
Oleksandr Grin <44158680+GrinOleksandr@users.noreply.github.com>
2019-03-08 13:14:25 +0200

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -12,60 +12,24 @@ self.addEventListener('install', function(event) {
     // установка
     event.waitUntil(
         caches.open(currentCacheName)
-            .then(function(cache) {
+            .then(cache => {
                 console.log('Opened cache');
                 return cache.addAll(filesToCache);
             })
-    );
-});
-
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
-        caches.keys()
-            .then(function (cachedNames) {
-                return Promise.all(
-                    cachedNames.filter(function (cacheName) {
-                        return cacheName !== currentCacheName;
-                    }).map(function (cacheName) {
-                        return caches.delete(cacheName);
-                    })
-                )
-            })
+            .then(() => self.skipWaiting())
     )
 });
 
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
 
-
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                // ресурс есть в кеше
-                if (response) {
-                    return response;
-                }
-
-                /* Важно: клонируем запрос. Запрос - это поток, может быть обработан только раз. Если мы хотим использовать объект request несколько раз, его нужно клонировать */
-                var fetchRequest = event.request.clone();
-
-                return fetch(fetchRequest).then(
-                    function(response) {
-                        // проверяем, что получен корректный ответ
-                        if(!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-
-                        /* ВАЖНО: Клонируем ответ. Объект response также является потоком. */
-                        var responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                            .then(function(cache) {
-                                cache.put(event.request, responseToCache);
-                            });
-
-                        return response;
-                    }
-                );
+        caches.open(cacheName)
+            .then(cache => cache.match(event.request, {ignoreSearch: true}))
+            .then(response => {
+                return response || fetch(event.request);
             })
     );
 });
