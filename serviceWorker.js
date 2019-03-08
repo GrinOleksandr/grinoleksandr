968d1d069c1bb8b9ae90a92ef5d97b734bb68b29
Oleksandr Grin <44158680+GrinOleksandr@users.noreply.github.com>
2019-03-08 13:03:42 +0200

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,40 +1,40 @@
-// let currentCacheName = 'myGitHub';
-//
-// let filesToCache = [
-//     '/index.html',
-//     '/js/main/js',
-//     '/img/AjaxLoader.gif',
-//     '/css/style.css',
-//     '/css/reset.css'
-// ];
-//
-// self.addEventListener('install', function(event) {
-//     event.waitUntil(
-//         caches.open(currentCacheName)
-//             .then(function (cache) {
-//                 return cache.addAll(filesToCache)
-//             })
-//     );
-// });
-//
-// self.addEventListener('activate', function(event) {
-//     event.waitUntil(
-//         caches.keys()
-//             .then(function (cachedNames) {
-//                 return Promise.all(
-//                     cachedNames.filter(function (cacheName) {
-//                         return cacheName !== currentCacheName;
-//                     }).map(function (cacheName) {
-//                         return caches.delete(cacheName);
-//                     })
-//                 )
-//             })
-//     )
-// });
-//
-//
-//
-//
+let currentCacheName = 'myGitHub';
+
+let filesToCache = [
+    '/index.html',
+    '/js/main/js',
+    '/img/AjaxLoader.gif',
+    '/css/style.css',
+    '/css/reset.css'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(currentCacheName)
+            .then(function (cache) {
+                return cache.addAll(filesToCache)
+            })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys()
+            .then(function (cachedNames) {
+                return Promise.all(
+                    cachedNames.filter(function (cacheName) {
+                        return cacheName !== currentCacheName;
+                    }).map(function (cacheName) {
+                        return caches.delete(cacheName);
+                    })
+                )
+            })
+    )
+});
+
+
+
+
 self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request)
