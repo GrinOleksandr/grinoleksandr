1825ba9c727a669ee34d132c17dbd7bf92fd7cb9
Oleksandr Grin <44158680+GrinOleksandr@users.noreply.github.com>
2019-03-08 14:35:47 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('./serviceWorker.js').then(function(registration) {
-            // Регистрация успешна
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }).catch(function(err) {
-            // Регистрация не успешна
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
+     navigator.serviceWorker.register('./serviceWorker.js')
 }
 
 
