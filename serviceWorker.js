9fdae1d1fbc5b5ecf7a0e370a25cc46accaada44
Oleksandr Grin <44158680+GrinOleksandr@users.noreply.github.com>
2019-03-08 12:54:03 +0200

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -45,16 +45,51 @@ self.addEventListener('activate', function(event) {
 //FAST TRACK END
 
 
+// self.addEventListener('fetch', function(event) {
+//     event.respondWith(
+//         caches.match(event.request)
+//             .then(function(response) {
+//                     // ресурс есть в кеше
+//                     if (response) {
+//                         return response;
+//                     }
+//                     return fetch(event.request);
+//                 }
+//             )
+//     );
+// });
+
+
 self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request)
             .then(function(response) {
-                    // ресурс есть в кеше
-                    if (response) {
+                // ресурс есть в кеше
+                if (response) {
+                    return response;
+                }
+
+                /* Важно: клонируем запрос. Запрос - это поток, может быть обработан только раз. Если мы хотим использовать объект request несколько раз, его нужно клонировать */
+                var fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest).then(
+                    function(response) {
+                        // проверяем, что получен корректный ответ
+                        if(!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        /* ВАЖНО: Клонируем ответ. Объект response также является потоком. */
+                        var responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function(cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
                         return response;
                     }
-                    return fetch(event.request);
-                }
-            )
+                );
+            })
     );
 });
